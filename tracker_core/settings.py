a1570412a740af1d21f426dcf502c7f340f68e59
Yaswanth1258 <yaswanthrajukomaragiri@gmail.com>
2026-09-25 12:02:51 +0530

    Settings updated

--- tracker_core/settings.py
+++ tracker_core/settings.py
@@ -75,15 +75,6 @@ WSGI_APPLICATION = 'tracker_core.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
-# Database configuration for production (PostgreSQL)
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
 
