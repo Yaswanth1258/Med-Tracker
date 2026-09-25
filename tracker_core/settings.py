17e298bdce06f8edfd5701423605a5b42435db55
Yaswanth1258 <yaswanthrajukomaragiri@gmail.com>
2026-09-25 12:28:04 +0530

    Fix database configuration for Render

--- tracker_core/settings.py
+++ tracker_core/settings.py
@@ -79,9 +79,25 @@ WSGI_APPLICATION = 'tracker_core.wsgi.application'
 
 
 # Database
+# Database
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+
+DATABASE_URL = os.environ.get("DATABASE_URL")
+
+if DATABASE_URL:
+    DATABASES = {
+        "default": dj_database_url.parse(
+            DATABASE_URL,
+            conn_max_age=600
+        )
+    }
+else:
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
+    }
 
 
 # Password validation
